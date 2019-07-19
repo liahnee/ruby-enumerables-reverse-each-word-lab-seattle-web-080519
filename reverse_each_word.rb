40492eb544f12a46890aac2b605b13a2c49789b8
Catherine Yang <liahnee@gmail.com>
2019-07-19 23:11:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(old)
   new = old.split(" ")
+  new1 = []
   new.each do |i|
-    new << i.reverse
+    new1 << i.reverse
   end
-  new1 =  new.join(" ")
+  new2 =  new1.join(" ")
   return new1
 end
