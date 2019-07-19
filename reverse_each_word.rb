3bbc028f994e0d3da9233c0d316c5b062a746fea
Catherine Yang <liahnee@gmail.com>
2019-07-19 23:14:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,21 @@ def reverse_each_word(old)
   new2 =  new1.join(" ")
   return new2
 end
+
+def reverse_each_word(old)
+  new = old.split(" ")
+  new.each_with_index do |j, i|
+    new[i] = j.reverse
+  end
+  new1 =  new.join(" ")
+  return new1
+end
+
+def reverse_each_word(old)
+  new = old.split(" ")
+  new = new.collect do |i|
+    i.reverse
+  end
+  new1 =  new.join(" ")
+  return new1
+end
