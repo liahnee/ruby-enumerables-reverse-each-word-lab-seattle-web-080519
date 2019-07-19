4af14f4bcee1cddbf48dfec4a578535caf712b35
Catherine Yang <liahnee@gmail.com>
2019-07-19 23:08:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,13 +8,3 @@ def reverse_each_word(old)
   new1 =  new.join(" ")
   return new1
 end
-
-
-def reverse_each_word(old)
-  new = old.split(" ")
-  new = new.collect do |i|
-    i.reverse
-  end
-  new1 =  new.join(" ")
-  return new1
-end
